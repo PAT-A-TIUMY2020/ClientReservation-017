e4334881922e489b52f7df72e133f604eebbe41d
Rayka Agustasya <raykaagustasya16@gmail.com>
2020-12-14 22:06:54 +0700

    Add project files.

---
+++ ClientReservation-017/ServiceReference1/Service1Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ServiceReference1
+{
+    internal class Service1Client
+    {
+        internal object pemesanan(string iDPemesanan, string namaCustomer, string noTelpon, int jumlahPemesanan, string idLokasi)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object pemesanan1()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object pemesanan(string iDPemesanan, string namaCustomer, string noTelpon)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object editPemesanan(string iDPemesanan, string namaCustomer, string noTelpon)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object deletePemesanan(string iDPemesanan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
